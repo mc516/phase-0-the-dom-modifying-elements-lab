b33f74484d1fe550bd13950704dd6ea3e8baad3a
Mickey <mickeyc11111@gmail.com>
2022-05-27 14:51:16 -0400

    commit tree 3659751bfd34495618b996f79309c7cdfa8c3630

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const Main = document.getElementById("main");
+Main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "He is the champion";
+document.body.append(newHeader);
